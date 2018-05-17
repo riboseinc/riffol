18c098e62aaea31a452a084c54eeaf3ed93dc40e
John Hedges <john@drystone.co.uk>
2018-05-17 16:45:32 +0100

    Adds dependencied (debian only)

--- src/config.rs
+++ src/config.rs
@@ -8,7 +8,8 @@ use std::collections::HashMap;
 struct JSONConfig {
     init: JSONInit,
     application_groups: HashMap<String, JSONApplicationGroup>,
-    applications: HashMap<String, JSONApplication>
+    applications: HashMap<String, JSONApplication>,
+    dependencies: HashMap<String, Vec<String>>
 }
 
 #[derive(Deserialize)]
@@ -18,7 +19,8 @@ struct JSONInit {
 
 #[derive(Deserialize)]
 struct JSONApplicationGroup {
-    applications: Vec<String>
+    applications: Vec<String>,
+    dependencies: Vec<String>
 }
 
 #[derive(Deserialize)]
@@ -38,6 +40,7 @@ fn default_application_restart() -> String { "restart".to_string() }
 
 pub struct Config {
     pub applications: Vec<Application>,
+    pub dependencies: Vec<String>
 }
 
 pub struct Application {
@@ -54,7 +57,7 @@ pub fn get_config<P: AsRef<Path>>(path: P) -> Result<Config, String> {
 	    , path.as_ref().display(), s))
     };
 
-    let mut config = Config { applications: vec![] };
+    let mut config = Config { applications: vec![], dependencies: vec![] };
     for group_name in json_config.init.application_groups {
         match json_config.application_groups.get(&group_name) {
 	    Some(group) => {
@@ -68,6 +71,12 @@ pub fn get_config<P: AsRef<Path>>(path: P) -> Result<Config, String> {
 				restart: ap.restart.clone()
 			    })},
 			None => return Err(format!("No such application \"{}\"", ap_name))
+		    }
+		}
+		for dep_name in &group.dependencies {
+		    match json_config.dependencies.get(dep_name) {
+		        Some(dep) => dep.iter().for_each(|d| config.dependencies.push(d.clone())),
+			None => return Err(format!("No such dependencies \"{}\"", dep_name))
 		    }
 		}
 	    },
--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,11 @@ extern crate riffol;
 
 use riffol::config::{get_config, Application};
 use std::process::Command;
-use std::env;
+use std::{env, thread, time};
 
 fn main() {
+    let arg0 = env::args().next().unwrap();
+
     let config_path = match env::args().skip(1).next() {
         Some(p) => p,
 	None => String::from("./riffol.conf")
@@ -13,23 +15,37 @@ fn main() {
     let config = match get_config(config_path) {
         Ok(c) => c,
 	Err(s) => {
-	    println!("{}", s);
+	    println!("{}: {}", arg0, s);
 	    return ();
 	}
     };
 
+    config.dependencies.iter().for_each(|d| {
+        let result = Command::new("apt-get")
+			     .arg("-y")
+			     .arg("--no-install-recommends")
+			     .arg("install")
+			     .arg(d)
+                             .status();
+        if result.is_err() || !result.unwrap().success() {
+            println!("{}: Failed to install dependency \"{}\"", arg0, d);
+            return ()
+	}
+    });
+
     let (running, failed)
     : (Vec<Option<&Application>>, Vec<Option<&Application>>)
     = config.applications.iter().map(|ap| {
-        let result = Command::new(format!("{} {}", ap.exec, ap.start))
+        let result = Command::new(&ap.exec)
+			     .arg(&ap.start)
 	                     .spawn();
         match result {
 	    Ok(_)  => {
-	        println!("Successfully spawned {}", ap.exec);
+	        println!("{}: Successfully spawned {}", arg0, ap.exec);
 		Some(ap)
 	    },
 	    Err(_) => {
-	        println!("Failed to spawn {}", ap.exec);
+	        println!("{}: Failed to spawn {}", arg0, ap.exec);
 	        None
 	    }
 	}
@@ -38,10 +54,16 @@ fn main() {
     if failed.len() != 0 {
         running.iter().rev().map(|o| o.unwrap()).for_each(|ap| {
             println!("Stopping {}", ap.exec);
-	    Command::new(format!("{} {}", ap.exec, ap.stop))
+	    Command::new(&ap.exec)
+	            .arg(&ap.stop)
 	            .spawn()
 		    .ok();
         });
 	return ();
+    }
+
+    let sleep_duration = time::Duration::from_secs(1);
+    loop {
+        thread::sleep(sleep_duration);
     }
 }
