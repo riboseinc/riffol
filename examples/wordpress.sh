9f107961dcb378a7eae8d42b75e106a8a175910d
drystone <john@drystone.co.uk>
2018-06-07 18:00:35 +0100

    Handles UCL configs as per spec
    * uses -f flag to specify configuration file
    
    * Uses nereon from crates.io

--- examples/wordpress.sh
+++ examples/wordpress.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
+NAME=riffol-wordpress
+UCLDIR=/usr/local/lib
+
 cd $(dirname $0)/..
 cargo build --release
 cp target/release/riffol examples/wordpress/
-docker build -t wordpress examples/wordpress
-docker run -t -i --rm -p 8080:80 wordpress
+cp $UCLDIR/libucl.so examples/wordpress/libucl.so
+docker build -t $NAME examples/wordpress
+docker volume create --name $NAME
+docker run -t -i --rm -p 8080:80 -v $NAME:/var/lib/mysql $NAME
