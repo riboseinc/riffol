e7e87fa39ee2438ac37f2d8bb2d4666b6e3c58ff
John Hedges <john@drystone.co.uk>
2018-06-08 13:16:50 +0100

    Fixes examples/wordpress
    * Don't re-initialise mysql on stop
    * Suppress apache error message

--- examples/wordpress/initialise-mysql.sh
+++ examples/wordpress/initialise-mysql.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-mysql --defaults-extra-file=/etc/mysql/debian.cnf </tmp/wordpress.sql >/dev/null 2>&1
+if [ "$1" = "start" ]; then
+    mysql --defaults-extra-file=/etc/mysql/debian.cnf </tmp/wordpress.sql >/dev/null 2>&1
+fi
 
 exit 0
