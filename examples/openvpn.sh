169c381ac38f4fb4aa9416de0adab80d4e1f3404
drystone <john@drystone.co.uk>
2018-06-27 15:07:10 +0100

    Supports per application environment variables
    * Support env = [] within application block

--- examples/openvpn.sh
+++ examples/openvpn.sh
@@ -5,15 +5,5 @@ localport=1194
 
 cd $(dirname $0)
 
-serial=$(cat openvpn/serial)
-
-if [ "x$riffolbranch" = "x" ]; then
-    riffolbranch=master
-fi
-
-if [ "x$nereondbranch" = "x" ]; then
-    nereondbranch=master
-fi
-
-docker build --build-arg serial=$serial --build-arg riffolbranch=$riffolbranch --build-arg nereondbranch=$nereondbranch -t $NAME openvpn
+docker build -t $NAME openvpn
 docker run -t -i --rm -p $localport:1194 --cap-add=NET_ADMIN -e "NEREON_FILESET=$(base64 -w 0 <openvpn/nereond.conf)" $NAME
