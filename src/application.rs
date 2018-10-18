4c266a382c36854bd013676647e7c4daf2001905
John Hedges <john@drystone.co.uk>
2018-10-18 16:55:56 +0100

    .

--- src/application.rs
+++ src/application.rs
@@ -103,7 +103,9 @@ impl Application {
 
     pub fn stop(&mut self) -> bool {
         let app_pid = self.get_app_pid();
-        if self.mode == Mode::Simple {
+        if self.mode == Mode::OneShot {
+            false
+        } else if self.mode == Mode::Simple {
             signal(app_pid.unwrap(), libc::SIGTERM);
             self.state = AppState::Stopping {
                 exec_pid: None,
